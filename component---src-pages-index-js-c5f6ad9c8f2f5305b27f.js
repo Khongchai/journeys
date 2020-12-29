0901a3ac30644aef64f7bf3adb05adec3310f0e0
Khongchai <world1955@hotmail.com>
2020-12-29 20:51:20 +0700

    Updates

--- component---src-pages-index-js-c5f6ad9c8f2f5305b27f.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{RXBc:function(n,t,e){"use strict";e.r(t),e.d(t,"default",(function(){return a}));var u=e("q1tI"),c=e.n(u),r=e("Kvkj");function a(){return c.a.createElement(r.a,null)}}}]);
-//# sourceMappingURL=component---src-pages-index-js-c5f6ad9c8f2f5305b27f.js.map
