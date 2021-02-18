c50b36d92d07b23852570547d4ac0496f2ec7692
khongchai <world1955@hotmail.com>
2021-02-19 00:56:06 +0700

    apply more consistent css to blog pages

--- src/elements/ContentElements.js
+++ src/elements/ContentElements.js
@@ -1,236 +1,202 @@
-import styled from "styled-components";
+import styled from "styled-components"
 
 export const ContentWrapper = styled.main`
+  grid-column: ${props => props.theme.width.mainContentWidth};
+  margin-left: 10px;
+  padding: 5.5rem;
+  padding-right: 1.6rem;
+  margin-left: 10px;
 
-    grid-column: ${props => props.theme.width.mainContentWidth};
-    margin-left: 10px;
-    padding: 5.5rem;
-    padding-right: 1.6rem;
-    margin-left: 10px;
+  z-index: 10;
 
-    z-index: 10;
-
-    /*
+  /*
         MDX style
     */
 
-    h1,
-    h2,h3,h4,h5,h6
-    {
-        margin-top: 2rem;
+  #article-container {
+    * + * {
+      margin-top: 1.5em;
     }
-    h1:first-child
-    {
-        margin-top: 3rem !important;
-    }
-    h1
-    {
-        font-size: 2rem;
-        line-height: 2.5rem;
-        font-weight: 400;
-    }
+  }
 
-    h2
-    {
-        font-size: 1.25rem;
-        line-height: 1.875rem;
-        font-weight: 400;
-    }
+  h1 {
+    font-size: 2rem;
+    line-height: 2.5rem;
+    font-weight: 400;
+  }
 
-    h3
-    {
-        font-size: 1.175rem;
-        line-height: 1.625rem;
-        font-weight: 700;
-    }
+  h2 {
+    font-size: 1.25rem;
+    line-height: 1.875rem;
+    font-weight: 400;
+  }
 
-    h4
-    {
-        font-size: 1rem;
-        line-height: 1.5rem;
-        font-weight: 400;
-    }
+  h3 {
+    font-size: 1.175rem;
+    line-height: 1.625rem;
+    font-weight: 700;
+  }
 
-    h5
-    {
-        font-size: 0.9rem;
-        line-height: 1.375rem;
-        font-weight: 700;
-    }
+  h4 {
+    font-size: 1rem;
+    line-height: 1.5rem;
+    font-weight: 400;
+  }
 
-    h6
-    {
-        font-size: 1rem;
-        line-height: 1.125rem;
-        font-weight: 700;
-        text-transform: uppercase;
-    }
+  h5 {
+    font-size: 0.9rem;
+    line-height: 1.375rem;
+    font-weight: 700;
+  }
 
-    p
-    {
-        font-size: 0.9rem;
-        line-height: 1.875rem;
-        font-weight: 400;
-        text-indent: 2.1rem;
-        margin-top: 1.245rem;
-    }
+  h6 {
+    font-size: 1rem;
+    line-height: 1.125rem;
+    font-weight: 700;
+    text-transform: uppercase;
+  }
 
-    a
-    {
-        
-       text-decoration: none; 
-    }
+  p {
+    font-size: 0.9rem;
+    line-height: 1.875rem;
+    font-weight: 400;
+    text-indent: 2.1rem;
+  }
 
-    strong /*sub section*/
-    {
-        display: block;
-        margin-top: 2rem;
-        font-weight: 700;
-    }
+  a {
+    text-decoration: none;
+  }
 
-    em
-    {
-        font-style: italic;
-    }
+  strong /*sub section*/ {
+    display: block;
 
-    del
-    {
-        text-decoration: line-through;
-    }
+    font-weight: 700;
+  }
 
-    blockquote p
-    {
-        font-style: italic;
-        background-color: ${props=>props.theme.colors.secondaryPink};
-        border-radius: 6px;
-        padding: 1rem;
-        line-height: 2.125rem;
-        max-width: 36rem;
-        margin:3rem auto;
-        :hover
-        {
-            box-shadow: rgba(46, 41, 51, 0.08) 0px 4px 16px, rgba(71, 63, 79, 0.16) 0px 8px 24px;
-        }
-        transition: box-shadow .3s;
-    }
-    em
-    {
-        font-size: 0.9rem;
-        display: block;
-    }
-    ul,ol
-    {
-        color: ${props => props.theme.colors.dark2};
-        margin: 1rem 0 1rem 2rem;
-    }
+  em {
+    font-style: italic;
+  }
 
-    li
-    {
-        margin: 0.25rem 0;
-    }
+  del {
+    text-decoration: line-through;
+  }
 
-    code
-    {
-      font-family: ${props => props.theme.fonts.code};
-      font-size: 1rem;
-      line-height: 1.875rem;  
-      color: ${props => props.theme.colors.light1};
-      background-color: ${props => props.theme.colors.dark3};
-      padding: 0 0.03rem;
+  blockquote p {
+    font-style: italic;
+    background-color: ${props => props.theme.colors.secondaryPink};
+    border-radius: 6px;
+    padding: 1rem;
+    line-height: 2.125rem;
+    max-width: 36rem;
+    margin: 3rem auto;
+    :hover {
+      box-shadow: rgba(46, 41, 51, 0.08) 0px 4px 16px,
+        rgba(71, 63, 79, 0.16) 0px 8px 24px;
     }
+    transition: box-shadow 0.3s;
+  }
+  em {
+    font-size: 0.9rem;
+    display: block;
+  }
+  ul,
+  ol {
+    color: ${props => props.theme.colors.dark2};
+    margin: 1rem 0 1rem 2rem;
+  }
 
-    hr
-    {
-        border: 0;
-        height: 1px;
-        background: ${props => props.theme.colors.dark1};
-        opacity: 0.1;
-        margin-top: 2rem;
-    }
+  li {
+    margin: 0.25rem 0;
+  }
 
-    table
-    {
-       width: 100%;
-       border-spacing: 0.25rem;
-       border-collapse: collapse;
-       font-size: 1rem;
-       line-height: 1.5rem;
-       font-weight: 400;
-    }
+  code {
+    font-family: ${props => props.theme.fonts.code};
+    font-size: 1rem;
+    line-height: 1.875rem;
+    color: ${props => props.theme.colors.light1};
+    background-color: ${props => props.theme.colors.dark3};
+    padding: 0 0.03rem;
+  }
 
-    th
-    {
-        font-weight: 700;
-    }
+  hr {
+    border: 0;
+    height: 1px;
+    background: ${props => props.theme.colors.dark1};
+    opacity: 0.1;
+  }
 
-    table,th,td
-    {
-        border: 1px solid ${props => props.theme.colors.dark3};
-    }
-    
-    th,td
-    {
-        text-align: left;
-        padding: 0.5rem;
-    }
+  table {
+    width: 100%;
+    border-spacing: 0.25rem;
+    border-collapse: collapse;
+    font-size: 1rem;
+    line-height: 1.5rem;
+    font-weight: 400;
+  }
 
-    @media ${props => props.theme.breakpoints.mobileAndTablet}
-    {
-        grid-column: 2 / span 6;
-        padding: 1rem;
-    }
+  th {
+    font-weight: 700;
+  }
 
-    img
-    {
-        transition: box-shadow .3s;
-        margin-top: 1rem;
-        width: 100%;
-        cursor: pointer;
-    }
-    img:hover
-    {
-        box-shadow: rgba(46, 41, 51, 0.08) 0px 4px 16px, rgba(71, 63, 79, 0.16) 0px 8px 24px;
-    }
-    .image-caption
-    {
-        text-align: right;
-    }
-    .adjacent
-    {
-        width: 40% !important;
-        margin: 1.2rem;
-    }
-    .adjacent-small
-    {
-        width: 20% !important;
-        margin: 1.2rem;
-    }
+  table,
+  th,
+  td {
+    border: 1px solid ${props => props.theme.colors.dark3};
+  }
 
-    .adjacent-images-container
-    {
-        display: flex;
-        align-items: center;
-    }
+  th,
+  td {
+    text-align: left;
+    padding: 0.5rem;
+  }
 
-    .large-image-contain
-    {
-        width: 70% !important;
-        margin-left: auto;
-        margin-right: auto;
-        display: block;
-    }
+  @media ${props => props.theme.breakpoints.mobileAndTablet} {
+    grid-column: 2 / span 6;
+    padding: 1rem;
+  }
 
-    .center
-    {
-        text-align: center !important;
-    }
+  img {
+    transition: box-shadow 0.3s;
+    margin-top: 1rem;
+    width: 100%;
+    cursor: pointer;
+  }
+  img:hover {
+    box-shadow: rgba(46, 41, 51, 0.08) 0px 4px 16px,
+      rgba(71, 63, 79, 0.16) 0px 8px 24px;
+  }
+  .image-caption {
+    text-align: right;
+  }
+  .adjacent {
+    width: 40% !important;
+    margin: 1.2rem;
+  }
+  .adjacent-small {
+    width: 20% !important;
+    margin: 1.2rem;
+  }
 
-    .half-width
-    {
-        width: 50% !important;
-        margin-left: auto;
-        margin-right: auto;
-        display: block;
-    }
+  .adjacent-images-container {
+    display: flex;
+    align-items: center;
+  }
 
-`;
+  .large-image-contain {
+    width: 70% !important;
+    margin-left: auto;
+    margin-right: auto;
+    display: block;
+  }
+
+  .center {
+    text-align: center !important;
+  }
+
+  .half-width {
+    width: 50% !important;
+    margin-left: auto;
+    margin-right: auto;
+    display: block;
+  }
+`
--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,54 +1,52 @@
-import React, {useEffect, useState} from "react";
-import { graphql } from "gatsby";
-import { MDXRenderer } from "gatsby-plugin-mdx";
-import { Content, SideContentBar, Footer, DynamicPagesContainer, EnlargedImageArea} from "../components";
-import SEO from "../components/Seo";
+import React, { useEffect, useState } from "react"
+import { graphql } from "gatsby"
+import { MDXRenderer } from "gatsby-plugin-mdx"
+import {
+  Content,
+  SideContentBar,
+  Footer,
+  DynamicPagesContainer,
+  EnlargedImageArea,
+} from "../components"
+import SEO from "../components/Seo"
 
-const DynamicBlog = ({data}) =>
-{
-    const [imageSRC, setImageSRC] = useState("");
-    useEffect(()=>{
-        let images = document.getElementsByClassName("enlargeable");
-        for (let i = 0; i < images.length; i++)
-        {
-            images[i].addEventListener("click", ()=>{
-                let imageArea = document.getElementById("image-area");
-                imageArea.style.display = "grid";
-                setImageSRC(images[i].src);
-            });
-        }
-    });
-    return(
-        <DynamicPagesContainer id="dynamic-page-container">
-        <SEO title={data.mdx.frontmatter.title}/>
-            <EnlargedImageArea imageSRC={imageSRC} />
-            <Content>
-            <div>
-                <MDXRenderer>
-                    {data.mdx.body}
-                </MDXRenderer>
-            </div>
-            </Content>
-            <SideContentBar/>
-            <Footer/>
-        </DynamicPagesContainer>
-    
-    );           
+const DynamicBlog = ({ data }) => {
+  const [imageSRC, setImageSRC] = useState("")
+  useEffect(() => {
+    let images = document.getElementsByClassName("enlargeable")
+    for (let i = 0; i < images.length; i++) {
+      images[i].addEventListener("click", () => {
+        let imageArea = document.getElementById("image-area")
+        imageArea.style.display = "grid"
+        setImageSRC(images[i].src)
+      })
+    }
+  })
+  return (
+    <DynamicPagesContainer id="dynamic-page-container">
+      <SEO title={data.mdx.frontmatter.title} />
+      <EnlargedImageArea imageSRC={imageSRC} />
+      <Content>
+        <div id="article-container">
+          <MDXRenderer>{data.mdx.body}</MDXRenderer>
+        </div>
+      </Content>
+      <SideContentBar />
+      <Footer />
+    </DynamicPagesContainer>
+  )
 }
 
-export default DynamicBlog;
+export default DynamicBlog
 
 export const pageQuery = graphql`
-    query BlogQuery ($blogID: String!)
-    {
-        mdx(id: {eq: $blogID})
-        {
-            body
-            frontmatter
-            {
-                title
-                slug
-            }
-        }
+  query BlogQuery($blogID: String!) {
+    mdx(id: { eq: $blogID }) {
+      body
+      frontmatter {
+        title
+        slug
+      }
     }
-`;
+  }
+`
